4efc9823ba86e72316315c6372e2ebbb543c714d
M-Muhaddis-Irshad <muhaddisirshad58@gmail.com>
2025-10-21 20:31:17 +0500

    Make home page completely responsive

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const supabaseApi = supabase.createClient('https://xyowgkiynvypiblztdjk.supabase
 // console.log(supabaseApi);
 
 // Initially check that User is loggedin or not_____________________________
+
+{
 // const isUserLoggedIn = async () => {
 //     const { data: {session}, error } = await supabaseApi.auth.getSession()
 
@@ -24,22 +26,23 @@ const supabaseApi = supabase.createClient('https://xyowgkiynvypiblztdjk.supabase
 // }
 
 // isUserLoggedIn()
+}
 
-{
 // NavBar toggling__________________________________________
 
-// const  body = document.getElementsByTagName("body");
-const navContainer = document.getElementById('navContainer');
-const toggleBar = document.getElementById('toggleBar');
-const navLinks = document.getElementById('navLinks');
+{
+    // const  body = document.getElementsByTagName("body");
+    const navContainer = document.getElementById('navContainer');
+    const toggleBar = document.getElementById('toggleBar');
+    const navLinks = document.getElementById('navLinks');
 
-toggleBar.addEventListener('click', () => {
-    navContainer.classList.toggle('active');
-    toggleBar.classList.toggle('active');
-    navLinks.classList.toggle('active');
-    
-    document.body.classList.toggle('active')
-})
+    toggleBar.addEventListener('click', () => {
+        navContainer.classList.toggle('active');
+        toggleBar.classList.toggle('active');
+        navLinks.classList.toggle('active');
+        document.body.classList.toggle('active')
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    })
 }
 
 // Array of objects with product details____________________________________________
@@ -347,78 +350,78 @@ const products = [
 
 // Product Cards creating_________________________________________________________
 
-// products.forEach((products, index) => {
+products.forEach((products, index) => {
 
-//     const { title, image, image2, name, about, reviews, price } = products
+    const { title, image, image2, name, about, reviews, price } = products
 
-//     container.innerHTML += `
-//             <div class="card" id="card-${index}">
+    container.innerHTML += `
+            <div class="card" id="card-${index}">
 
-//                 <a class="text-decoration-none text-black" href="allPages/product/product.html?title=${title}">
+                <a class="text-decoration-none text-black" href="allPages/product/product.html?title=${title}">
 
-//                     <div class="img_div" id='image-${index}' style="background-image: url(${image});">
-//                     </div>
-//                     <div class="product_content">
+                    <div class="img_div" id='image-${index}' style="background-image: url(${image});">
+                    </div>
+                    <div class="product_content">
 
-//                         <div class="prdctName_n_Hrt">
-//                             <h6 class="prdctName">${name}</h6>
-                            
-//                             <!-- <i class="cart material-icons" id="cart-${index}" title="Add"> shopping_cart </i> -->
-                            
-//                             <i class="cart material-icons" id="cart-${index}"  title="Like">favorite</i>
-//                         </div>
+                        <div class="prdctName_n_Hrt">
+                            <h6 class="prdctName">${name}</h6>
 
-//                         <div class="aboutProduct">${about}</div>
+                            <!-- <i class="cart material-icons" id="cart-${index}" title="Add"> shopping_cart </i> -->
 
-//                         <div class="starRevwCnt">
-//                             <span class="stars">
-//                                 <i class="star material-icons">star</i>
-//                                 <i class="star material-icons">star</i>
-//                                 <i class="star material-icons">star</i>
-//                                 <i class="star material-icons">star</i>
-//                                 <i class="starHalf material-icons">star_half</i>
-//                             </span>
-//                             <span class="prdct_Review_Count">(${reviews} Reviews)</span>
-//                         </div>
+                            <i class="cart material-icons" id="cart-${index}"  title="Like">favorite</i>
+                        </div>
 
-//                         <div class="prdctPrice">Rs ${price}</div>
-//                     </div>
+                        <div class="aboutProduct">${about}</div>
 
-//                 </a>
+                        <div class="starRevwCnt">
+                            <span class="stars">
+                                <i class="star material-icons">star</i>
+                                <i class="star material-icons">star</i>
+                                <i class="star material-icons">star</i>
+                                <i class="star material-icons">star</i>
+                                <i class="starHalf material-icons">star_half</i>
+                            </span>
+                            <span class="prdct_Review_Count">(${reviews} Reviews)</span>
+                        </div>
 
-//             </div>
+                        <div class="prdctPrice">Rs ${price}</div>
+                    </div>
 
-//     `
+                </a>
 
-//     setTimeout(() => {
+            </div>
 
-//         // Cart Icon fill_________________________________________________________________
-//         const heart = document.getElementById(`cart-${index}`)
+    `
 
-//         heart.addEventListener('click', (event) => {
-//             event.preventDefault(); // Stops link navigation like (a)
-//             event.stopPropagation(); // Stops bubbling
-//             heart.classList.toggle('fill');
-//             heart.title = heart.classList.contains('fill') ? 'Remove' : 'Add';
-//         });
+    setTimeout(() => {
+
+        // Cart Icon fill_________________________________________________________________
+        const heart = document.getElementById(`cart-${index}`)
+
+        heart.addEventListener('click', (event) => {
+            event.preventDefault(); // Stops link navigation like (a)
+            event.stopPropagation(); // Stops bubbling
+            heart.classList.toggle('fill');
+            heart.title = heart.classList.contains('fill') ? 'Remove' : 'Add';
+        });
 
 
-//         // // Cards hover effect_____________________________________________________________________---
-//         const card = document.getElementById(`card-${index}`);
-//         const img = document.getElementById(`image-${index}`);
+        // // Cards hover effect_____________________________________________________________________---
+        const card = document.getElementById(`card-${index}`);
+        const img = document.getElementById(`image-${index}`);
 
-//         card.addEventListener('mouseover', () => {
-//             img.style.backgroundImage = `url(${image2})`;
-//             img.classList.add('transformImg');
-//         })
-//         card.addEventListener('mouseout', () => {
-//             img.style.backgroundImage = `url(${image})`;
-//             img.classList.remove('transformImg');
-//         })
+        card.addEventListener('mouseover', () => {
+            img.style.backgroundImage = `url(${image2})`;
+            img.classList.add('transformImg');
+        })
+        card.addEventListener('mouseout', () => {
+            img.style.backgroundImage = `url(${image})`;
+            img.classList.remove('transformImg');
+        })
 
-//     }, 0);
+    }, 0);
 
-// });
+});
 
 
 // NewsLetter_____________________________________________________________
@@ -451,7 +454,19 @@ function SubscribeToNewsLetter() {
         Swal.fire({
             icon: "success",
             title: "Subscription confirmed!",
-            text: " You’ll now receive our latest news, tips, and updates straight to your email."
+            text: " You’ll now receive our latest news, tips, and updates straight to your email.",
+            showConfirmButton: false,
+            timer: 4000,
+            timerProgressBar: true,    // shows progress line at top
+            allowOutsideClick: true,   // let user click outside to close
+            allowEscapeKey: true,       // allow ESC key to close.
+            didOpen: () => {
+                const progressBar = Swal.getTimerProgressBar();
+                if (progressBar) {
+                    progressBar.style.background = "dodgerblue"; // 💙 color for progress bar
+                    progressBar.style.height = "6px";            // slightly thicker line
+                }
+            }
         });
         subscribeInput.value = "";
     }
